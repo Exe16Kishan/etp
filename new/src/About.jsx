c9d57195de13f7e371bbd7e8b996f035dc03a0b3
Kishan <kishanshukla53@gmail.com>
2024-12-02 15:10:12 +0530

    init

---
+++ new/src/About.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const About = () => {
+    return <h1>About Us: Learn more about our mission and vision.</h1>;
+};
+
+export default About;
---
+++ new/src/Contact.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Contact = () => {
+    return <h1>Contact Us: We would love to hear from you!</h1>;
+};
+
+export default Contact;
